9f5cac11d6663f3b16cb01c6dd12f4d3bfeb2903
danielgo98 <daniel.andres1998@gmail.com>
2024-05-11 16:44:51 +0200

    Update: created search heroe

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './search-page.component.css'
 })
 export class SearchPageComponent {
+
+  constructor(private heroesService: HeroesService) {}
+
+  searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  searchHero(): void {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value).subscribe(heroe => this.heroes = heroe);
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+
+    if(!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
+
+  }
 
 }
